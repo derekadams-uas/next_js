2c8431724cbf7399e750c4cdef25ffaddea180b6
Derek Adams <derekadams.uas@gmail.com>
2020-01-09 21:29:18 -0700

    adding index and about

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+const Index = () => (
+    <div>
+        <h1>SSR MAGICIAN</h1>
+        <Link href='/about'>
+            <a>About</a>
+        </Link>
+        {/* <a href='./about'>About</a> */}
+    </div>
+);
+
+export default Index;
